499cf73d21b71865e2fbcdb0c21ef905127a5336
inclue <wb092311@naver.com>
2021-11-07 05:11:05 +0900

    Model 구현 완료

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize');
-const University = require('./university');
-const Member = require('./member');
+const Comment = require('./comment');
+const Divison = require('./divison');
 const Mail = require('./mail');
+const Member = require('./member');
+const Menu = require('./menu');
+const Restaurant = require('./restaurant');
+const University = require('./university');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -16,12 +20,28 @@ db.University = University;
 University.init(sequelize);
 University.associate(db);
 
+db.Mail = Mail;
+Mail.init(sequelize);
+Mail.associate(db);
+
 db.Member = Member;
 Member.init(sequelize);
 Member.associate(db);
 
-db.Mail = Mail;
-Mail.init(sequelize);
-Mail.associate(db);
+db.Divison = Divison;
+Divison.init(sequelize);
+Divison.associate(db);
+
+db.Restaurant = Restaurant;
+Restaurant.init(sequelize);
+Restaurant.associate(db);
+
+db.Menu = Menu;
+Menu.init(sequelize);
+Menu.associate(db);
+
+db.Comment = Comment;
+Comment.init(sequelize);
+Comment.associate(db);
 
 module.exports = db;
